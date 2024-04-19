98a4e23a42212a629699fc705e4b3d2aa3a2f498
alex-kropps <167565942+alex-kropps@users.noreply.github.com>
2024-04-19 20:21:23 +0300

    666

--- js/slider-init.js
+++ js/slider-init.js
@@ -1,12 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     var sliders = document.querySelectorAll('.photos-slider');
-    sliders.forEach(function (slider) {
-        new Swiper(slider, {
-            loop: true,
-            spaceBetween: 20,
-            slidesPerView: 'auto',
-            centeredSlides: true,
-            // Дополнительные настройки Swiper
+
+    sliders.forEach(function(slider) {
+        $(slider).slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            dots: true,
+            autoplay: false,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false
+                    }
+                }
+            ]
         });
     });
 });
